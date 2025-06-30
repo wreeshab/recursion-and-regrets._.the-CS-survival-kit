236ec66571fd1a040c5ebab15652b5e560a2afe6
Vrishabhanath E <137403592+wreeshab@users.noreply.github.com>
2025-06-30 22:07:53 +0530

    Create setmatrixzewr.cpp

---
+++ dsa/striver/array/setmatrixzewr.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+
+                    if(j == 0){
+                        col0 =0;
+                    }else{
+                        matrix[0][j] =0;
+                    }
+                }
+            }
+        }
+        for(int i = 1;i< n;i++){
+            for(int  j = 1;j<m;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            } 
+        }
+
+        //top row
+        if(matrix[0][0] == 0){
+            for(int i = 1;i< m;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        // first column, this should be done last because , it has the least possiblility of getting overridden.
+        if(col0 == 0){
+            for(int i = 0;i< n;i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
