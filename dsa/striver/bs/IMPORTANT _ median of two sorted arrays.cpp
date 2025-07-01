d0534d652a47dd0a8682e0ddc8dd9d86e715332e
Vrishabhanath E <137403592+wreeshab@users.noreply.github.com>
2025-07-01 17:49:29 +0530

    Create IMPORTANT _ median of two sorted arrays.cpp

---
+++ dsa/striver/bs/IMPORTANT _ median of two sorted arrays.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1); // Ensure nums1 is smaller
+
+        int low = 0, high = n1;
+        int total = n1 + n2;
+        int left = (total + 1) / 2;
+
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
+            int r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
+            int r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (total % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+            else if (l1 > r2)
+                high = mid1 - 1;
+            else
+                low = mid1 + 1;
+        }
+
+        return 0.0; // Unreachable in valid input
+    }
+};
