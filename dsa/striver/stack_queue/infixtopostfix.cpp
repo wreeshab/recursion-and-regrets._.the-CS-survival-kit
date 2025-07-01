9588f6657e54fc05d17aab3b868247744c20cd21
Vrishabhanath E <137403592+wreeshab@users.noreply.github.com>
2025-07-01 12:57:04 +0530

    Create infixtopostfix.cpp

---
+++ dsa/striver/stack_queue/infixtopostfix.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prec(char c) {
+    if (c == '^')
+        return 3;
+    else if (c == '/' || c == '*')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
+string infixToPostfix(string s) {
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        // If the scanned character is
+        // an operand, add it to the output string.
+
+        // isalnum can be used as well.
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+            res += c;
+
+        // If the scanned character is an
+        // ‘(‘, push it to the stack.
+        else if (c == '(')
+            st.push('(');
+
+        // If the scanned character is an ‘)’,
+        // pop and add to the output string from the stack
+        // until an ‘(‘ is encountered.
+        else if (c == ')') {
+            while (st.top() != '(') {
+                res += st.top();
+                st.pop();
+            }
+            st.pop(); 
+        }
+
+        // If an operator is scanned
+        else {
+            while (!st.empty() && prec(c) <= prec(st.top())) {
+                res += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    // Pop all the remaining elements from the stack
+    while (!st.empty()) {
+        res += st.top();
+        st.pop();
+    }
+
+    return res;
+}
+
+int main() {
+    string exp = "a+b*(c^d-e)^(f+g*h)-i";
+    cout << infixToPostfix(exp);
+    return 0;
+}
