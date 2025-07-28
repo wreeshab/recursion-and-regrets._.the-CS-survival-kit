b560fc0683a269b4c37ee1c7599b57bf06ddb534
wreeshab <evrishab@gmail.com>
2025-07-28 20:57:01 +0530

    test from windows

--- dsa/striver/array/rotate_array.cpp
+++ dsa/striver/array/rotate_array.cpp
@@ -17,3 +17,17 @@ public:
         return;
     }
 };
+// for right rotate , separate as n-k and k.
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k %n;
+
+        reverse(nums.begin(), nums.begin()+n-k);
+        reverse(nums.begin()+n-k, nums.end());
+        reverse(nums.begin() , nums.end());
+    }
+};
+
+// for left rotate, separate as k and n - k;
